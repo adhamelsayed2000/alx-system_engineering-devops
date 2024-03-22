c908ffb4e5a8856faf5f7187251a579ed4ee3c06
adhamelsayed2000 <adhamadhoom9@gamil.com>
2024-03-22 11:29:08 +0200

    done

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,5 +1,6 @@
-# Define Python package resource for Flask
-python::pip { 'flask':
-  ensure    => '2.1.0',
-  provider  => 'pip3',
+#!/usr/bin/pup
+# Install an especific version of flask (2.1.0)
+package {'flask':
+  ensure   => '2.1.0',
+  provider => 'pip3'
 }
