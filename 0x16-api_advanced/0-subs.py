8fdd8dd82831296edc7182aa47af10b289e2afe5
adhamelsayed2000 <adhamadhoom9@gamil.com>
2024-05-07 15:32:14 +0300

    edit2

--- 0x16-api_advanced/0-subs.py
+++ 0x16-api_advanced/0-subs.py
@@ -1,6 +1,8 @@
 #!/usr/bin/python3
 """
-queries the Reddit API 
+queries the Reddit API and returns the number of subscribers (not active users,
+total subscribers) for a given subreddit. If an invalid subreddit is given,
+return 0
 """
 from requests import get
 from sys import argv
